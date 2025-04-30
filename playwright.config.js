0841d32351ca3354064089fa47fd2bcb42d5c302
Sachin-Monotype <sachin.koirala@monotype.com>
2025-04-30 09:23:24 +0545

    added allure report method

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,27 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests', // Directory where your test files are located
+  testDir: './UI_Automation/tests', // Directory where your test files are located
   retries: 1, // Retry failed tests once
   use: {
-    headless: true, // Run tests in headless mode
+    headless: false, // Run tests in headless mode
     baseURL: 'https://www.saucedemo.com', // Base URL for tests
     trace: 'on-first-retry', // Collect trace on first retry
     screenshot: 'on', // Take screenshots on both success and failure
+    video: 'retain-on-failure', // Optional: Retain video on failure
   },
   reporter: [
     ['list'], // Default console reporter
-    ['allure-playwright'], // Allure reporter
+    ['allure-playwright', { outputFolder: 'test-results/allure-results' }], // Save Allure results here
   ],
   outputDir: 'test-results', // Directory for test results
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        screenshot: 'on',
+        outputDir: 'test-results/screenshots/', // Save screenshots here
+      },
+    },
+  ],
 });
